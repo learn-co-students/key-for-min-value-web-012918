9a5d67721f79c92145201e5a67e9dff5f0d251e0
Brooke Yalof <brookeyalof2017@u.northwestern.edu>
2018-01-18 02:25:06 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  my_min = nil
+  my_return = nil
+
+  name_hash.each do |obj, num|
+
+    if my_min == nil || num < my_min
+      my_min = num
+      my_return = obj
+    end
+
+  end
+
+  my_return
+
 end
